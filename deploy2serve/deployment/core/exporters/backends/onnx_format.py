Add precision estimation for onnx into benchmark step, fix bug in onnx transfer, update req for sapiens.

deploy2serve/deployment/core/exporters/backends/onnx_format.py
@@ -2,11 +2,12 @@ from abc import abstractmethod
 from contextlib import contextmanager
 from copy import deepcopy
 from pathlib import Path
+import numpy as np
 import os
 import onnx
 import onnxslim
 import torch
-from typing import Any, Dict, Optional, Tuple
+from typing import Any, Dict, Optional, Tuple, List
 
 from deploy2serve.deployment.core.exporters.base import BaseExporter, ExportConfig, ExporterFactory
 from deploy2serve.deployment.models.export import Backend
@@ -60,7 +61,7 @@ class ONNXExporter(BaseExporter):
                 "arena_extend_strategy": "kSameAsRequested",
                 "cudnn_conv_algo_search": "HEURISTIC",
                 "do_copy_in_default_stream": True,
-                "enable_cuda_graph": False,
+                "enable_cuda_graph": True,
                 "enable_skip_layer_norm_strict_mode": True,
                 "use_tf32": True,
             }
@@ -71,14 +72,32 @@ class ONNXExporter(BaseExporter):
 
         session, input_names, output_names = ORTExecutor.load(self.save_path, sess_options, default_provider)
         layer_info = next(self.model.parameters())
-        placeholder = torch.ones((1, 3, *self.config.input_shape), dtype=layer_info.dtype).numpy()
+        placeholder = torch.ones((1, 3, *self.config.input_shape), dtype=layer_info.dtype)
 
         self.logger.info(f"Benchmark on tensor with shapes: {tuple(placeholder.shape)}")
         with timer(self.logger, self.config.repeats, warmup_iterations=50, cuda_profiling=False) as t:
-            t(lambda: session.run(output_names, {input_names[0]: placeholder}))
+            t(lambda: session.run(output_names, {input_names[0]: placeholder.numpy()}))
+
+        original_output = self.model(placeholder.to(self.config.device))
+        onnx_output = session.run(output_names, {input_names[0]: placeholder.numpy()})
+
+        if isinstance(original_output, torch.Tensor):
+            original_output = [original_output.detach().cpu().numpy()]
+        elif isinstance(original_output, List):
+            original_output = [item.detach().cpu().numpy() for item in original_output]
+        else:
+            TypeError("Type of return value from pytorch model must be 'torch.Tensor' or equal Sequence of them.")
+
+        for i, (torch_out, onnx_out) in enumerate(zip(original_output, onnx_output)):
+            if not np.allclose(torch_out, onnx_out, rtol=1e-3, atol=1e-5):
+                max_diff = np.max(np.abs(torch_out - onnx_out))
+                self.logger.warning(f"Output {output_names[i]} is NOT close! Max diff: {max_diff:.4f}.")
+            else:
+                self.logger.info(f"Output {output_names[i]}  is numerically close to PyTorch output.")
 
     def export(self) -> None:
         if os.path.exists(self.save_path) and not self.config.onnx.force_rebuild:
+            self.logger.info(f"ONNX model already exists at {self.save_path}. Skipping export.")
             return
 
         self.logger.info("Try convert PyTorch model to ONNX format")
@@ -86,20 +105,23 @@ class ONNXExporter(BaseExporter):
         layer_info = next(model.parameters())
         placeholder = torch.zeros((1, 3, *self.config.input_shape), dtype=layer_info.dtype, device=layer_info.device)
         options = self.config.onnx.specific.model_dump()
-        with self.patch_ops():
-            torch.onnx.export(model, (placeholder,), str(self.save_path), **options)
-        self.register_onnx_plugins()
+        try:
+            with self.patch_ops():
+                torch.onnx.export(model, (placeholder,), str(self.save_path), **options)
+            self.register_onnx_plugins()
+            onnx.checker.check_model(self.save_path, full_check=True)
 
-        onnx_model = onnx.load(self.save_path)
-        onnx.checker.check_model(onnx_model, full_check=True)
-        if self.config.onnx.simplify:
-            self.logger.info("Try to simplify ONNX model")
-            optimized_onnx_model = onnxslim.slim(
-                onnx_model,
-                model_check=True,
-                skip_optimizations=False,
-                skip_fusion_patterns=False
-            )
-            onnx.save_model(optimized_onnx_model, self.save_path)
-            self.logger.info("Simplification successfully done")
+            if self.config.onnx.simplify:
+                self.logger.info("Try to simplify ONNX model")
+                optimized_onnx_model = onnxslim.slim(
+                    str(self.save_path),
+                    skip_optimizations=False,
+                    skip_fusion_patterns=False
+                )
+                onnx.checker.check_model(optimized_onnx_model, full_check=True)
+                onnx.save_model(optimized_onnx_model, self.save_path)
+                self.logger.info("Simplification successfully done")
+        except Exception as error:
+            self.logger.critical(f"Catch error while apply export: {error}")
+
         self.logger.info(f"ONNX model successfully stored in: {self.save_path}")